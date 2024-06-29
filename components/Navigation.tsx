bf9c6fffd47cd44ecf5bd85f1d4a7477e40d8f46
Toby Liu <ybot1122@gmail.com>
2024-06-28 19:04:22 -0700

    make initial website

---
+++ components/Navigation.tsx
@@ -0,0 +1,15 @@
+export default function Navigation() {
+  return (
+    <nav className="absolute top-0 left-0 z-10 text-white">
+      <div className="inline-block text-2xl mt-5 mx-10">Just Be Still</div>
+
+      <ul className="inline-block">
+        <li className="inline-block ml-5">About</li>
+        <li className="inline-block ml-5">Team</li>
+        <li className="inline-block ml-5">Services</li>
+        <li className="inline-block ml-5">Blog</li>
+        <li className="inline-block ml-5">Contact</li>
+      </ul>
+    </nav>
+  );
+}
