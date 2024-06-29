846f7f4602146aa79d23ecf76f3ec729fc448fb7
Toby Liu <ybot1122@gmail.com>
2024-06-29 10:26:30 -0700

    Finished contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Contact() {
+  return (
+    <>
+      <div className="relative w-full h-[100vh] min-h-[1024px] items-center justify-between text-sm bg-forest">
+        <div className="text-center mt-[100px] p-5 max-w-[1024px] mx-auto">
+          <h1 className="text-3xl md:text-6xl text-white mb-10">Contact Us</h1>
+
+          <div className="text-xl md:text-2xl text-white mb-5">
+            Interested in a business inquiry?
+            <br />
+            Feel free to email us at:{" "}
+            <span className="font-bold">email@email.com</span>
+          </div>
+
+          <div className="text-xl md:text-2xl text-white mb-5">
+            Want to reach us sooner?
+            <br />
+            Please call us: <span className="font-bold">123-456-7890</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
