9ca542ed717dcd36778678ebaf4b70520bce17ad
Toby Liu <ybot1122@gmail.com>
2024-06-28 19:18:15 -0700

    More tweaks to page

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,19 @@ export default function Home() {
           </h2>
         </div>
       </div>
+      <div className="text-center w-full">
+        <h2 className="text-forest text-6xl mt-20 mb-5">Our approach</h2>
+        <p className="text-2xl color-text max-w-[1024px] leading-loose mb-20 px-5 m-auto">
+          We are a group of doctoral-level psychologists and psychiatrists who
+          provide quality mental health care. As a mental health collective, we
+          assist members by providing therapy, medication management, coaching,
+          and more. Let us help you connect with one of our doctors who meets
+          your needs and is available to see you, online or in-person.
+        </p>
+      </div>
+      <div className="text-center bg-forest w-full">
+        <h2 className="text-white text-6xl mt-20 mb-5">Our Services</h2>
+      </div>
     </>
   );
 }
