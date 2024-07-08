bca6b7faeb6022c329751acc5538a3cb588dd598
Toby Liu <ybot1122@gmail.com>
2024-07-08 15:54:11 -0700

    Homepage updates

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 import logo from "@/public/logo.jpg";
 import home_bg from "@/public/home_Bg.jpg";
 import down_icon from "@/public/down-arrow.png";
+import home_pic1 from "@/public/homepic1.jpeg";
 
 import { Indie_Flower } from "next/font/google";
 
-const poppins = Indie_Flower({
+const indie_flower = Indie_Flower({
   subsets: ["latin"],
   weight: ["400"],
   style: ["normal"],
@@ -48,39 +49,50 @@ export default function Home() {
         </div>
       </div>
       <div className="flex flex-wrap justify-center items-center text-center w-full h-[100vh]">
-        <h2 className="text-forest text-4xl md:text-6xl mt-20 mx-5 w-full">
+        <h2 className={`text-forest text-4xl md:text-6xl mt-20 mx-5 w-full`}>
           Welcome to Just Be Still
         </h2>
         <div className="w-full">
           <p className="text-2xl max-w-[1024px] md:leading-loose inline-block px-5">
-            Welcome to Just Be Still, a creative fashion design group dedicated
-            to inspiring the next generation of designers! We specialize in
-            teaching kids how to sew and unleash their creativity.
+            A creative fashion design group dedicated to inspiring the next
+            generation of designers! We specialize in teaching kids how to sew
+            and unleash their creativity.
           </p>
         </div>
-        <div className="w-full">
-          <p className="text-2xl max-w-[1024px] md:leading-loose inline-block px-5">
-            <Link href="/about" className="underline text-forest">
-              Learn More About Us
-            </Link>
-          </p>
+        <div className="w-full px-5">
+          <div className="relative max-w-[1024px] max-h-[50vh] m-auto aspect-video">
+            <Image
+              src={home_pic1}
+              placeholder="blur"
+              alt="Just Be Still Class"
+              fill
+              objectFit="contain"
+            />
+          </div>
         </div>
         <div className="mb-5 animate-bounce grow self-end">
           Scroll Down to see our services
         </div>
       </div>
-      <div className="flex flex-col flex-wrap justify-center items-center text-center w-full h-[100vh] bg-forest">
-        <h2 className="text-white text-4xl md:text-6xl mt-20 mx-5 w-full">
+      <div className="flex flex-wrap justify-center items-center text-center w-full h-[100vh] bg-forest text-white">
+        <h2 className=" text-4xl md:text-6xl mt-20 px-5 w-full">
           Our Services
         </h2>
-        <ul className="text-left text-white text-2xl mx-5 mt-10">
-          <li>Sewing Classes</li>
-          <li>Private Sewing Classes</li>
-          <li>Alterations</li>
-          <li>Custom Order</li>
-          <li>Sewing Parties</li>
-          <li>Service Projects</li>
-        </ul>
+        <div className="w-full">
+          <ul className="text-2xl max-w-[1024px] md:leading-loose inline-block px-5">
+            <li>Sewing Classes</li>
+            <li>Private Sewing Classes</li>
+            <li>Alterations</li>
+            <li>Custom Order</li>
+            <li>Sewing Parties</li>
+            <li>Service Projects</li>
+          </ul>
+        </div>
+        <p className="text-2xl max-w-[1024px] md:leading-loose inline-block px-5 flex-1">
+          <Link href="/services" className="underline text-white">
+            Click Here for More Details
+          </Link>
+        </p>
       </div>
     </>
   );
