ed399506ba5e98c42410800d5b5e3fda805200a4
Toby Liu <ybot1122@gmail.com>
2024-07-08 14:46:35 -0700

    Updates to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/public/logo.jpg";
+import home_bg from "@/public/home_Bg.jpg";
+import down_icon from "@/public/down-arrow.png";
 
 import { Indie_Flower } from "next/font/google";
 
@@ -16,11 +18,12 @@ export default function Home() {
       <div className="relative w-full h-[100vh] items-center justify-between text-sm">
         <div className="flex justify-center items-center w-full h-full before:absolute before:block before:w-full before:h-full before:z-10 before:bg-recipeHeroScrim">
           <Image
-            src="/home_Bg.jpg"
+            src={home_bg}
             alt="Just Be Still"
             fill
             sizes="100vw"
             objectFit="cover"
+            placeholder="blur"
           />
           <div className="text-white z-20">
             <Link href="/">
@@ -29,20 +32,40 @@ export default function Home() {
                 alt="Just Be Still Logo"
                 width={250}
                 height={250}
+                sizes="100vw"
               />
             </Link>
           </div>
+          <div className="absolute bottom-0 mb-5 animate-bounce z-10">
+            <Image
+              src={down_icon}
+              alt="Scroll Down"
+              sizes="100vw"
+              width={50}
+              height={50}
+            />
+          </div>
         </div>
       </div>
-      <div className="text-center w-full">
-        <h2 className="text-forest text-6xl mt-20 mb-10">Our approach</h2>
-        <p className="text-2xl color-text max-w-[1024px] leading-loose mb-20 px-5 m-auto">
-          We are a group of doctoral-level psychologists and psychiatrists who
-          provide quality mental health care. As a mental health collective, we
-          assist members by providing therapy, medication management, coaching,
-          and more. Let us help you connect with one of our doctors who meets
-          your needs and is available to see you, online or in-person.
-        </p>
+      <div className="flex flex-wrap justify-center items-center text-center w-full h-[100vh]">
+        <h2 className="text-forest text-4xl md:text-6xl mt-20 mx-5 w-full">
+          Welcome to Just Be Still
+        </h2>
+        <div className="w-full">
+          <p className="text-2xl max-w-[1024px] md:leading-loose inline-block px-5">
+            Welcome to Just Be Still, a creative fashion design group dedicated
+            to inspiring the next generation of designers! We specialize in
+            teaching kids how to sew and unleash their creativity.
+          </p>
+        </div>
+        <div className="w-full">
+          <p className="text-2xl max-w-[1024px] md:leading-loose inline-block px-5">
+            <Link href="/about" className="underline text-forest">
+              Learn More About Us
+            </Link>
+          </p>
+        </div>
+        <div className="mb-5 animate-bounce grow self-end">more?</div>
       </div>
       <div className="text-center bg-forest w-full pb-10">
         <h2 className="text-white text-6xl mt-20 mb-20">Our Services</h2>
