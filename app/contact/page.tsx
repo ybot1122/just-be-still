e7ebc325e1421acbd13682d1a64399e58b73f90f
Toby Liu <ybot1122@gmail.com>
2024-07-10 12:55:52 -0700

    Events and Contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,6 @@
 import PageHeader from "@/components/PageHeader";
+import PageParagraph from "@/components/PageParagraph";
+import PageSection from "@/components/PageSection";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,22 +8,24 @@ export default function Contact() {
   return (
     <>
       <PageHeader header="Contact" />
-      <div className="text-center w-full max-w-[1024px] mx-auto ">
-        <div className="px-5 md:px-20">
-          <p className="text-xl md:text-2xl color-text md:leading-loose mt-5 md:mt-20 px-5 m-auto">
-            <div className="text-xl md:text-2xl mb-5">
-              Reach us on{" "}
-              <Link
-                href={"https://www.facebook.com/profile.php?id=61550921396070"}
-                className="underline"
-              >
-                Facebook
-              </Link>
-              !
-            </div>
+      <PageSection>
+        <PageParagraph>
+          <p className="text-left mb-5">
+            Email us at{" "}
+            <span className="underline">justbestilldesign@gmail.com</span>
           </p>
-        </div>
-      </div>
+          <p className="text-left mb-5">Phone: 515-338-0447</p>
+          <p className="text-left">
+            Visit us on{" "}
+            <Link
+              href={"https://www.facebook.com/profile.php?id=61550921396070"}
+              className="underline"
+            >
+              Facebook
+            </Link>
+          </p>
+        </PageParagraph>
+      </PageSection>
     </>
   );
 }
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,14 +1,40 @@
 import PageHeader from "@/components/PageHeader";
-import PageParagraph from "@/components/PageParagraph";
 import PageSection from "@/components/PageSection";
 import Image from "next/image";
+import julyevent from "@/public/events/july2024.png";
+import PageParagraph from "@/components/PageParagraph";
 
 export default function Events() {
   return (
     <>
       <PageHeader header="Events" />
       <PageSection>
-        <PageParagraph>Hi</PageParagraph>
+        <div className="relative w-full mt-5">
+          <Image
+            src={julyevent}
+            alt="Event in July"
+            placeholder="blur"
+            sizes="100vw"
+            width={0}
+            height={0}
+            style={{
+              width: "100%",
+              height: "auto",
+            }}
+          />
+        </div>
+      </PageSection>
+      <PageSection>
+        <PageParagraph isAccent>
+          <p className="">Coming Up</p>
+          <p className="text-left">
+            <span className="font-bold">Weekend Workshops</span> in September,
+            October, November
+          </p>
+          <p className="text-left">
+            <span className="font-bold">Service Project</span> in December
+          </p>
+        </PageParagraph>
       </PageSection>
     </>
   );
