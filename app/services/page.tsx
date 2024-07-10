dc5809b3e805296df95fd3ceb669885e5e874817
Toby Liu <ybot1122@gmail.com>
2024-07-10 11:06:46 -0700

    Fix lint

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -4,6 +4,9 @@ import PageSection from "@/components/PageSection";
 import ServiceItem from "@/components/ServiceItem";
 import Link from "next/link";
 
+import private_lessons from "@/public/private_lessons.jpg";
+import Image from "next/image";
+
 export default function Contact() {
   return (
     <>
@@ -16,13 +19,13 @@ export default function Contact() {
           </ServiceItemParagraph>
           <ServiceItemParagraph>
             Join us for a fun and creative experience where kids can learn the
-            art of sewing and unleash their creativity. Whether they're
+            art of sewing and unleash their creativity. Whether they&apos;re
             beginners or have some experience, our classes are designed to
             inspire and teach valuable skills.
           </ServiceItemParagraph>
           <ServiceItemParagraph>
-            Don't miss out on this opportunity to nurture the next generation of
-            designers. Sign up today and let the creativity begin!
+            Don&apos;t miss out on this opportunity to nurture the next
+            generation of designers. Sign up today and let the creativity begin!
           </ServiceItemParagraph>
           <ServiceItemParagraph>
             <Link
@@ -38,33 +41,42 @@ export default function Contact() {
             We also offer private lessons for those who prefer one-on-one
             instruction tailored to their specific needs and interests. Private
             classes are available by appointment. Everything will be provided.
+            <div className="relative w-[250px] md:w-[450px] aspect-square mx-auto mt-10">
+              <Image
+                src={private_lessons}
+                alt="Private Lessons"
+                placeholder="blur"
+                objectFit="contain"
+                fill
+              />
+            </div>
           </ServiceItemParagraph>
         </ServiceItem>
         <ServiceItem q="Alterations">
           <ServiceItemParagraph>
-            Do you have pants that are too long or a skirt that's too big for
-            your waist, and you've been relying on a safety pin to make it work?
-            It's time to alter them to your size! Alterations are available by
-            appointment.
+            Do you have pants that are too long or a skirt that&apos;s too big
+            for your waist, and you&apos;ve been relying on a safety pin to make
+            it work? It&apos;s time to alter them to your size! Alterations are
+            available by appointment.
           </ServiceItemParagraph>
         </ServiceItem>
         <ServiceItem q="Custom Order">
           <ServiceItemParagraph>
             We make blankets, including fidget and weighted ones, for kids with
-            special needs. Plush blankets are made with your child's favorite
-            plush; fleece blankets can be made from your child's favorite print.
-            Have a baby shower to attend? We can make something special for you.
-            Have a parent living with Alzheimer's or dementia? We can create a
-            busy lapbook specifically for them. Need it personalized? We can do
-            that too.
+            special needs. Plush blankets are made with your child&apos;s
+            favorite plush; fleece blankets can be made from your child&apos;s
+            favorite print. Have a baby shower to attend? We can make something
+            special for you. Have a parent living with Alzheimer&apos;s or
+            dementia? We can create a busy lapbook specifically for them. Need
+            it personalized? We can do that too.
           </ServiceItemParagraph>
         </ServiceItem>
         <ServiceItem q="Sewing Parties">
           <ServiceItemParagraph>
-            Looking for something unique for your kids' party? Try a sewing
+            Looking for something unique for your kids&apos; party? Try a sewing
             party! No matter the skill level, everyone can join in. Minimum
-            length is 2 hours with a maximum of 12 kids. We'll consider themes
-            and project choices. Everything will be provided.
+            length is 2 hours with a maximum of 12 kids. We&apos;ll consider
+            themes and project choices. Everything will be provided.
           </ServiceItemParagraph>
         </ServiceItem>
         <ServiceItem q="Service Projects" isLast>
