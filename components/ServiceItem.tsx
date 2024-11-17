99de76d12791f47ea0791595c9248175ff0cb574
Toby Liu <ybot1122@gmail.com>
2024-11-16 21:05:10 -0800

    Add tobyUI FAQ

--- components/ServiceItem.tsx
+++ components/ServiceItem.tsx
@@ -1,46 +1,23 @@
 "use client";
 
-import { useState } from "react";
+import { FAQItem } from "@ybot1122/toby-ui/FAQItem";
 
 export default function ServiceItem({
   q,
   children,
-  isLast = false,
 }: {
   q: string;
-  children: React.ReactNode;
-  isLast?: boolean;
+  children: JSX.Element | JSX.Element[];
 }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const content = isOpen ? children : null;
-
   return (
-    <div
-      className={` flex py-10 border-t border-borderGray text-left w-full flex-wrap ${isLast ? "border-b" : ""}`}
+    <FAQItem
+      question={q}
+      questionFontColor="text-forest"
+      fillColor="fill-forest"
+      borderColor="border-borderGray"
+      bold
     >
-      <button
-        className={`font-bold text-forest block text-blue text-xl md:text-2xl cursor w-1/2 text-left ${isOpen ? "mb-10" : ""} grow`}
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        {q}
-      </button>
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className={`${isOpen ? "mb-10" : ""} ml-5 relative`}
-        aria-hidden="true"
-      >
-        <rect
-          className="right-[20px] absolute inline-block w-[20px] h-[2px] bg-forest"
-          aria-hidden="true"
-        ></rect>
-        <rect
-          className={`right-[20px] inline-block w-[20px] h-[2px] bg-forest absolute ${isOpen ? "animate-closePlus" : "rotate-90 animate-openPlus"}`}
-          aria-hidden="true"
-        ></rect>
-      </button>
-
-      {content}
-    </div>
+      {children}
+    </FAQItem>
   );
 }
