413b208ee13595de815da0cc41bd04e6cc6c01fd
Toby Liu <ybot1122@gmail.com>
2024-12-09 20:42:47 -0800

    ignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: ['sveltekit-version/']
+    ignoreDuringBuilds: ['sveltekit-version/', 'sveltekit-version/**']
    },
  
 };
