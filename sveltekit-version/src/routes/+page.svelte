4dd0102e9c84f8607fe7901346b506118236fa89
Toby Liu <ybot1122@gmail.com>
2024-12-09 21:03:43 -0800

    Home page hero

--- sveltekit-version/src/routes/+page.svelte
+++ sveltekit-version/src/routes/+page.svelte
@@ -1,6 +1,12 @@
+<script lang="ts">
+	import HomePageHeroImageSection from '../components/HomePageHeroImageSection/HomePageHeroImageSection.svelte';
+</script>
+
 <main>
 	<h1>Welcome to SvelteKit</h1>
-	<p>
-		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-	</p>
+	<HomePageHeroImageSection>
+    <div class="text-white z-20 my-[150px] tall:my-0">
+      hi
+    </div>
+  </HomePageHeroImageSection>
 </main>
