99df1bc28dd8c503994718c38ab794c6789bb99a
Toby Liu <ybot1122@gmail.com>
2024-12-12 15:49:32 -0800

    Removed main wrapper

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="w-full mt-[25px] mb-5 text-xs text-center">
-      <p>&copy; 2024 JustBeStill</p>
-      <p className="mt-2">
-        Visit us on{" "}
+      <p>
+        <span className="mr-5">&copy; 2024 JustBeStillDesign</span>Visit us on{" "}
         <Link
           href={"https://www.facebook.com/profile.php?id=61550921396070"}
-          className="underline"
+          className="underline mr-5"
         >
           Facebook
         </Link>
-      </p>
-      <p className="underline mt-2 text-[0.5rem]">
-        <Link href="https://www.flaticon.com/free-icons/group">
+        <Link
+          href="https://www.flaticon.com/free-icons/group"
+          className="text-xs"
+        >
           Icons by Freepik
         </Link>
       </p>
--- components/HomePageMain.tsx
+++ components/HomePageMain.tsx
@@ -16,132 +16,107 @@ const whiteBgInd = [1, 4];
 export default function HomePageMain() {
   return (
     <>
-      <Navigation forestFont={true} fixed />
-      <main>
-        <HomePageHeroImageSection src={home_bg} alt="JustBeStill">
-          <div className="text-white z-20 my-[150px] tall:my-0">
-            <Link href="/">
-              <Image
-                src={logo}
-                alt="JustBeStill Logo"
-                width={250}
-                height={250}
-                sizes="100vw"
-              />
-            </Link>
-          </div>
-        </HomePageHeroImageSection>
-
-        <HomePageInfoSection header="Welcome to JustBeStill">
-          <div className="w-full">
-            <p className="text-xl md:text-2xl max-w-[1024px] md:leading-loose inline-block px-5">
-              A creative fashion design group dedicated to inspiring the next
-              generation of designers! We specialize in teaching kids how to sew
-              and unleash their creativity.
-            </p>
-          </div>
-          <div className="w-full px-5">
-            <div className="relative max-w-[1024px] max-h-[50vh] m-auto aspect-video">
-              <Image
-                src={home_pic1}
-                placeholder="blur"
-                alt="JustBeStill Class"
-                fill
-                objectFit="contain"
-              />
-            </div>
-          </div>
-          <div className="mb-5 animate-bounce grow self-end mt-5 tall:mt-0">
-            <button className="underline">
-              Interested in a sewing party? &#127881;
-            </button>
-          </div>
-        </HomePageInfoSection>
-
-        <HomePageHeroImageSection src={home_pic2} alt="JustBeStill Events">
-          <div className="text-white z-20 text-4xl md:text-6xl text-center px-10 md:px-0 my-[150px] tall:my-0">
-            Sewing Classes &amp;
-            <br />
-            &#127881; Parties &#127881;
-            <br />
-            Available
-            <p className="text-2xl md:text-4xl underline mt-5">
-              {" "}
-              <Link href="/events">See our Events</Link>
-            </p>
-          </div>
-        </HomePageHeroImageSection>
-
-        <HomePageInfoSection isForest header={"Meet the Instructor"}>
-          <div className="w-full px-5">
-            <div className="relative max-w-[1024px] max-h-[35vh] md:max-h-[50vh] m-auto aspect-square">
-              <Image
-                src={home_pic3}
-                placeholder="blur"
-                alt="JustBeStill Class"
-                fill
-                objectFit="contain"
-              />
-            </div>
-          </div>
-
-          <p className="text-2xl max-w-[1024px] md:leading-loose inline-block px-5 w-full mt-20 tall:mt-0">
-            <Link href="/about" className="underline text-forest bg-white p-5">
-              Learn about us
-            </Link>
-          </p>
-          <p className="w-full md:leading-loose inline-block px-5 animate-bounce mt-20 tall:mt-0 mb-5 tall:mb-0">
-            <button className="underline">Explore our services</button>
-          </p>
-        </HomePageInfoSection>
-
-        <HomePageInfoSection header="Our Services">
-          <div className="grid grid-cols-3 gap-2 w-full">
-            <HomePageServicesIcon serviceName="Sewing Classes" icon="sewing" />
-            <HomePageServicesIcon
-              serviceName="Private Sewing Classes"
-              icon="needle"
+      <HomePageHeroImageSection src={home_bg} alt="JustBeStill">
+        <div className="text-white z-20 my-[150px] tall:my-0">
+          <Link href="/">
+            <Image
+              src={logo}
+              alt="JustBeStill Logo"
+              width={250}
+              height={250}
+              sizes="100vw"
             />
-            <HomePageServicesIcon serviceName="Alterations" icon="fabric" />
-            <HomePageServicesIcon serviceName="Custom Order" icon="gift" />
-            <HomePageServicesIcon serviceName="Sewing Parties" icon="party" />
-            <HomePageServicesIcon
-              serviceName="Service Projects"
-              icon="sewing-machine"
+          </Link>
+        </div>
+      </HomePageHeroImageSection>
+
+      <HomePageInfoSection header="Welcome to JustBeStill">
+        <div className="w-full">
+          <p className="text-xl md:text-2xl max-w-[1024px] md:leading-loose inline-block px-5">
+            A creative fashion design group dedicated to inspiring the next
+            generation of designers! We specialize in teaching kids how to sew
+            and unleash their creativity.
+          </p>
+        </div>
+        <div className="w-full px-5">
+          <div className="relative max-w-[1024px] max-h-[50vh] m-auto aspect-video">
+            <Image
+              src={home_pic1}
+              placeholder="blur"
+              alt="JustBeStill Class"
+              fill
+              objectFit="contain"
             />
           </div>
-          <p className="text-2xl max-w-[1024px] md:leading-loose inline-block self-end bg-forest text-white p-5 mb-10">
-            <Link href="/services" className="underline">
-              See All our Services
-            </Link>
-          </p>
-        </HomePageInfoSection>
+        </div>
+        <div className="mb-5 animate-bounce grow self-end mt-5 tall:mt-0">
+          <button className="underline">
+            Interested in a sewing party? &#127881;
+          </button>
+        </div>
+      </HomePageInfoSection>
 
-        <HomePageInfoSection isForest>
-          <div>
-            <PageParagraph isAccent>
-              <Link href="/contact" className="underline">
-                Contact Us
-              </Link>
-            </PageParagraph>
-            <p className="mt-5">&copy; 2024 JustBeStill</p>
-            <p className="mt-2">
-              Visit us on{" "}
-              <Link
-                href={"https://www.facebook.com/profile.php?id=61550921396070"}
-                className="underline"
-              >
-                Facebook
-              </Link>
-            </p>
-            <p className="underline mt-2 text-[0.5rem] w-full mb-5 tall:mb-0">
-              <Link href="https://www.flaticon.com/free-icons/group">
-                Icons by Freepik
-              </Link>
-            </p>
+      <HomePageHeroImageSection src={home_pic2} alt="JustBeStill Events">
+        <div className="text-white z-20 text-4xl md:text-6xl text-center px-10 md:px-0 my-[150px] tall:my-0">
+          Sewing Classes &amp;
+          <br />
+          &#127881; Parties &#127881;
+          <br />
+          Available
+          <p className="text-2xl md:text-4xl underline mt-5">
+            {" "}
+            <Link href="/events">See our Events</Link>
+          </p>
+        </div>
+      </HomePageHeroImageSection>
+
+      <HomePageInfoSection isForest header={"Meet the Instructor"}>
+        <div className="w-full px-5">
+          <div className="relative max-w-[1024px] max-h-[35vh] md:max-h-[50vh] m-auto aspect-square">
+            <Image
+              src={home_pic3}
+              placeholder="blur"
+              alt="JustBeStill Class"
+              fill
+              objectFit="contain"
+            />
           </div>
-        </HomePageInfoSection>
-      </main>
+        </div>
+
+        <p className="text-2xl max-w-[1024px] md:leading-loose inline-block px-5 w-full mt-20 tall:mt-0">
+          <Link href="/about" className="underline text-forest bg-white p-5">
+            Learn about us
+          </Link>
+        </p>
+        <p className="w-full md:leading-loose inline-block px-5 animate-bounce mt-20 tall:mt-0 mb-5 tall:mb-0">
+          <button className="underline">Explore our services</button>
+        </p>
+      </HomePageInfoSection>
+
+      <HomePageInfoSection header="Our Services">
+        <div className="grid grid-cols-3 gap-2 w-full">
+          <HomePageServicesIcon serviceName="Sewing Classes" icon="sewing" />
+          <HomePageServicesIcon
+            serviceName="Private Sewing Classes"
+            icon="needle"
+          />
+          <HomePageServicesIcon serviceName="Alterations" icon="fabric" />
+          <HomePageServicesIcon serviceName="Custom Order" icon="gift" />
+          <HomePageServicesIcon serviceName="Sewing Parties" icon="party" />
+          <HomePageServicesIcon
+            serviceName="Service Projects"
+            icon="sewing-machine"
+          />
+        </div>
+        <p className="text-2xl max-w-[1024px] md:leading-loose inline-block self-end text-white p-5 mb-10">
+          <Link href="/services" className="underline bg-forest p-5 mr-5">
+            See All our Services
+          </Link>
+          <Link href="/contact" className="underline ml-5 bg-forest p-5">
+            Contact Us
+          </Link>
+        </p>
+      </HomePageInfoSection>
     </>
   );
 }
