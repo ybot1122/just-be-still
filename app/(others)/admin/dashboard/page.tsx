d1ae28ac44022af5eeabe75ce4971d2e3799e320
Toby Liu <ybot1122@gmail.com>
2024-12-31 16:10:51 -0800

    Get images from Cloudinary

--- app/(others)/admin/dashboard/page.tsx
+++ app/(others)/admin/dashboard/page.tsx
@@ -4,6 +4,7 @@ import checkAuth from "@/server_actions/checkAuth";
 import { redirect } from "next/navigation";
 import Tabs from "./Tabs";
 import getPageData from "@/lib/getPageData";
+import { getCloudinaryImages } from "@/server_actions/getCloudinaryImages";
 
 export default async function AdminDashboard() {
   const isAuthed = await checkAuth();
@@ -13,6 +14,8 @@ export default async function AdminDashboard() {
   }
 
   const data = await getPageData();
+
+  const images = await getCloudinaryImages("just-be-still-design");
 
   return (
     <>
--- server_actions/getCloudinaryImages.ts
+++ server_actions/getCloudinaryImages.ts
@@ -2,10 +2,24 @@
 
 import { CLOUDINARY_CLOUD_NAME } from "@/constants/cloudinary";
 
-export async function getCloudinaryImages(folder: string): Promise<any[]> {
+type CloudinaryResource = {
+  asset_id: string;
+  public_id: string;
+  resource_type: string;
+  format: string;
+  folder: string;
+  url: string;
+  secure_url: string;
+};
+
+export async function getCloudinaryImages(
+  folder: string,
+): Promise<CloudinaryResource[]> {
   const CLOUDINARY_KEY = process.env.CLOUDINARY_KEY;
   const CLOUDINARY_SECRET = process.env.CLOUDINARY_SECRET;
-  const URL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/resources`;
+  const URL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/resources/image`;
+
+  const result: CloudinaryResource[] = [];
 
   try {
     const response = await fetch(URL, {
@@ -14,10 +28,16 @@ export async function getCloudinaryImages(folder: string): Promise<any[]> {
       },
     });
     const data = await response.json();
-    console.log(data);
-    return data;
+
+    for (let i = 0; i < data.resources.length; i++) {
+      const resource = data.resources[i];
+      if (resource.folder.includes(folder)) {
+        result.push(resource);
+      }
+    }
   } catch (error) {
     console.error("Error fetching images from Cloudinary:", error);
     throw error;
   }
+  return result;
 }
