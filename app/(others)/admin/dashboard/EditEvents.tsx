5d4f7725d16fbd60f16266729c40b88d7f5566c8
Toby Liu <ybot1122@gmail.com>
2025-01-01 18:14:58 -0800

    making a form

--- app/(others)/admin/dashboard/EditEvents.tsx
+++ app/(others)/admin/dashboard/EditEvents.tsx
@@ -1,33 +1,41 @@
 import BasicButton from "@/components/admin/BasicButton";
 import { Content_Event } from "@/content/events";
 import { useImageChooser } from "@/context/ImageChooserContext";
-import React, { Dispatch, SetStateAction, useState } from "react";
+import { updatePageContent } from "@/server_actions/updatePageContent";
+import React, { Dispatch, SetStateAction, useCallback, useState } from "react";
 
 const EditEvents = ({ events }: { events: Content_Event }) => {
   const [poster, setPoster] = useState(events.poster);
 
+  const submitForm = useCallback(async (e: FormData) => {
+    const update = await updatePageContent("test", JSON.stringify(e));
+  }, []);
+
   return (
     <div className="text-left">
-      <div className="mt-5">
-        <h2 className="text-xl">Poster</h2>
-        <ImageUploader original={events.poster} />
-      </div>
-      <div className="mt-5">
-        <h2 className="text-xl">Update the Extra Images</h2>
-        <p>Content for section 2</p>
-      </div>
-      <div className="mt-5">
-        <h2 className="text-xl">Update the Banner Message</h2>
-        <p>Content for section 3</p>
-      </div>
+      <form action={submitForm}>
+        <div className="mt-5">
+          <h2 className="text-xl">Poster</h2>
+          <ImageUploader original={events.poster} />
+        </div>
+        <div className="mt-5">
+          <h2 className="text-xl">Update the Extra Images</h2>
+          <p>Content for section 2</p>
+        </div>
+        <div className="mt-5">
+          <h2 className="text-xl">Update the Banner Message</h2>
+          <p>Content for section 3</p>
+        </div>
+        <div className="mt-5">
+          <BasicButton type="submit">Update Page</BasicButton>
+        </div>
+      </form>
     </div>
   );
 };
 
 const ImageUploader = ({ original }: { original: Content_Event["poster"] }) => {
-  const [selectedImage, setSelectedImage] = useState<File | string>(
-    original.path,
-  );
+  const [selectedImage, setSelectedImage] = useState<string>(original.path);
   const { setCallback: setImageChooserCb } = useImageChooser();
 
   return (
@@ -60,34 +68,25 @@ const ImageUploader = ({ original }: { original: Content_Event["poster"] }) => {
           </BasicButton>
         </div>
       </div>
+      <input type="hidden" value={selectedImage} name="poster" />
     </div>
   );
 };
 
 const EditableText = ({ text }: { text: string }) => {
-  const [isEditing, setIsEditing] = useState(false);
   const [value, setValue] = useState(text);
 
   const className =
     "border border-gray-300 p-2 cursor-text inline-block w-[400px] ml-2";
 
-  const handleBlur = () => {
-    setIsEditing(false);
-  };
-
-  return isEditing ? (
+  return (
     <input
       type="text"
       value={value}
       onChange={(e) => setValue(e.target.value)}
-      onBlur={handleBlur}
       className={className}
       autoFocus
     />
-  ) : (
-    <span onClick={() => setIsEditing(true)} className={className}>
-      {value} <span className="text-blue-500">click to edit</span>
-    </span>
   );
 };
 
