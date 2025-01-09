24087c400751180a0d9452cba520a4e97a9b8357
Toby Liu <ybot1122@gmail.com>
2025-01-08 16:51:32 -0800

    some padding

--- app/(others)/events/EventsCarousel.tsx
+++ app/(others)/events/EventsCarousel.tsx
@@ -29,7 +29,7 @@ export default function EventsCarousel() {
 
 const CarouselImage = ({ src, alt }: { src: string; alt: string }) => {
   return (
-    <div className="relative h-[300px] sm:h-[400px] md:h-[500px] overflow-hidden">
+    <div className="relative h-[300px] sm:h-[400px] md:h-[500px] overflow-hidden px-5">
       <img
         src={src}
         alt={alt}
