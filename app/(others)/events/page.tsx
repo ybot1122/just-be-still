ba01cebfc1c63f01fe7e761bdb19c1c2c6401f23
Toby Liu <ybot1122@gmail.com>
2025-01-08 16:01:59 -0800

    add content

--- app/(others)/events/page.tsx
+++ app/(others)/events/page.tsx
@@ -11,28 +11,27 @@ export default async function Events() {
   return (
     <>
       <PageHeader header="Events" />
-      <PageSection>
-        <div className="relative w-full mt-5">
-          <img
-            src={poster.path}
-            alt={poster.alt}
-            className="w-full h-full object-cover"
-          />
-        </div>
-      </PageSection>
 
       <PageSection>
-        <div className="flex mt-5 md:mt-20 gap-2">
-          {extras.map((extra) => (
-            <div key={extra.path} className="max-h-[350px]">
-              <img
-                src={extra.path}
-                alt={extra.alt}
-                className="w-full h-full object-cover"
-              />
-            </div>
-          ))}
-        </div>
+        <PageParagraph as="div" className="font-bold">
+          December Workshop Update
+        </PageParagraph>
+        <PageParagraph as="div" className="text-left">
+          This month, we had the joy of partnering with Green Hills Retirement
+          Home to bring a little extra comfort and love to their residents. With
+          55 residents eagerly awaiting handmade pillows, we got to work and
+          created 55 beautiful, cozy pillows just for them!
+        </PageParagraph>
+        <PageParagraph as="div" className="text-left">
+          Delivering these heartfelt gifts was an unforgettable experience.
+          Seeing their smiles and hearing their words of gratitude reminded us
+          why we do what we do. Our hearts are full knowing we could spread some
+          love and make them feel remembered this holiday season
+        </PageParagraph>
+        <PageParagraph as="div" className="text-left">
+          Thank you to everyone who helped make this possible—you're part of the
+          magic!
+        </PageParagraph>
       </PageSection>
 
       <PageSection>
--- components/PageParagraph.tsx
+++ components/PageParagraph.tsx
@@ -2,17 +2,19 @@ export default function PageParagraph({
   children,
   isAccent = false,
   as = "p",
+  className = "",
 }: {
   children: React.ReactNode;
   isAccent?: boolean;
   as?: "p" | "div";
+  className?: string;
 }) {
   const bgClass = isAccent ? ` bg-forest text-white py-5 ` : "";
   const Component = as;
 
   return (
     <Component
-      className={`text-xl md:text-2xl md:leading-loose mt-5 md:mt-20 px-5 m-auto ${bgClass}`}
+      className={`text-xl md:text-2xl md:leading-loose mt-5 md:mt-20 px-5 m-auto ${bgClass} ${className}`}
     >
       {children}
     </Component>
