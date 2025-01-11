e8126fec7d484882858c174cd3c82847647a1f45
Toby Liu <ybot1122@gmail.com>
2025-01-10 19:08:51 -0800

    codemod cookies

--- server_actions/auth.ts
+++ server_actions/auth.ts
@@ -16,7 +16,9 @@ export default async function auth(pass: string) {
       const at = encryptSymmetric(ENCRYPTION_KEY, pass);
       const val = `${at.ciphertext}.${at.tag}.${at.iv}`;
 
-      await cookies().set(COOKIES_ADMIN_TOKEN, val, {
+      await (
+        await cookies()
+      ).set(COOKIES_ADMIN_TOKEN, val, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         maxAge: COOKIES_AGE_2_HOURS,
