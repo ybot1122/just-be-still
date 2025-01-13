e58edbd748679f66ddbe3404d247acaec5f77344
Toby Liu <ybot1122@gmail.com>
2025-01-13 15:00:43 -0800

    handle failed build

--- app/(others)/admin/dashboard/ChangesSubmitted.tsx
+++ app/(others)/admin/dashboard/ChangesSubmitted.tsx
@@ -4,25 +4,25 @@ import React from "react";
 import VercelDeploymentEvents from "./VercelDeploymentEvents";
 import { Loader } from "@/components/Loader";
 import BasicButton from "@/components/admin/BasicButton";
+import FieldError from "@/components/admin/FieldError";
 
 const ChangesSubmitted: React.FC = () => {
   const [deploymentId, setDeploymentId] = React.useState(null);
   const [deploymentDone, setDeploymentDone] = React.useState(false);
+  const [error, setError] = React.useState(false);
+
   React.useEffect(() => {
     const fetchData = async () => {
       const response = await fetch("/admin/listDeployments");
       const result = await response.json();
 
-      const status = result.deployments[0].readyState;
+      // @todo NOTE we are not checking this deployment was triggered
+      // specifically by this publish. That should be implemented.
 
-      if (status === "READY") {
-        setTimeout(fetchData, 5000);
-      } else {
-        setDeploymentId(result.deployments[0].uid);
-      }
+      setDeploymentId(result.deployments[0].uid);
     };
 
-    fetchData();
+    setTimeout(fetchData, 5000);
   }, []);
 
   const color = deploymentDone ? "text-green-500" : "";
@@ -46,6 +46,10 @@ const ChangesSubmitted: React.FC = () => {
               </BasicButton>
             </p>
           </>
+        ) : error ? (
+          <div className="text-red-500 font-bold">
+            The deployment failed. Please contact web admin.
+          </div>
         ) : (
           <Loader color="#000" width={50} />
         )}
@@ -55,6 +59,7 @@ const ChangesSubmitted: React.FC = () => {
         <VercelDeploymentEvents
           id={deploymentId}
           setDeploymentDone={() => setDeploymentDone(true)}
+          setError={() => setError(true)}
         />
       ) : null}
     </div>
